25c8130c1e30a0be20e805fa7b2c8e5a37e0589f
Yuriy <qa.yuris2015@gmail.com>
2023-08-07 00:19:23 +0300

    adding code for test in scope of homework # 22

--- hw22/test_hw22.py
+++ hw22/test_hw22.py
@@ -1,19 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import os
 import requests
 import json
-import os
-from selenium.webdriver.support.select import Select
 
 
 class TestUserCreateThenLoginThenCarAddAndAfterAllDelete:
 
     def setup_class(self):
-        self.session = requests.Session()  # Create a session to persist cookies
-        # Get the directory of the current script
+        self.session = requests.Session()
         self.script_dir = os.path.dirname(os.path.abspath(__file__))
         self.driver_path = os.path.join(self.script_dir, "C:/webdrivers/chromedriver.exe")
-        # Construct the path to the JSON file using os.path.join()
         json_file_path = os.path.join(self.script_dir, "test_data.json")
         with open(json_file_path) as config:
             self.json_data = json.load(config)
@@ -21,56 +19,57 @@ class TestUserCreateThenLoginThenCarAddAndAfterAllDelete:
         self.password = self.json_data['password']
 
     def test_registration_user(self):
-        """
-        Function for user creation
-        :return:
-        """
         result = self.session.post(url='https://qauto2.forstudy.space/api/auth/signup', json=self.json_data)
         assert result.json()["status"] == "ok"
 
-    def test_login_add_car_and_then_delete_all(self):
-        """
-        Function for login, add a car, get car id, then delete car and user
-        :return:
-        """
-        self.driver = webdriver.Chrome(self.driver_path)
+    def user_login(self):
+        chrome_service = webdriver.chrome.service.Service(self.driver_path)
+        self.driver = webdriver.Chrome(service=chrome_service)
+
         self.driver.get("https://guest:welcome2qauto@qauto2.forstudy.space/")
         self.driver.implicitly_wait(2)
-        # user login
+
         sign_in_button = self.driver.find_element(By.XPATH, "//button[text()='Sign In']")
         sign_in_button.click()
+
         email_field = self.driver.find_element(By.ID, "signinEmail")
         password_field = self.driver.find_element(By.ID, "signinPassword")
         email_field.send_keys(self.email)
         password_field.send_keys(self.password)
+
         self.driver.find_element(By.XPATH, "//button[text()='Login']").click()
-        # add car
+
+    def test_add_car(self):
+        self.user_login()
+
         add_car_button = self.driver.find_element(By.XPATH, "//button[text()='Add car']")
         add_car_button.click()
-        # select brand
+
         drop_down_select_brand = self.driver.find_element(By.ID, "addCarBrand")
         select_brand = Select(drop_down_select_brand)
         select_brand.select_by_index(3)
-        # put mileage
+
         mileage = self.driver.find_element(By.ID, "addCarMileage")
         mileage.send_keys("100000")
-        # select model
-        drop_down_select_brand = self.driver.find_element(By.ID, "addCarModel")
-        select_brand = Select(drop_down_select_brand)
-        select_brand.select_by_index(1)
-        # add car
+
+        # drop_down_select_model = self.driver.find_element(By.ID, "addCarModel")
+        # select_model = Select(drop_down_select_model)
+        # select_model.select_by_index(1)
+
         add_button = self.driver.find_element(By.XPATH, "//button[text()='Add']")
         add_button.click()
         self.driver.close()
-        # get car id
+
+    def test_delete_car_by_id(self):
         result_car_id = self.session.get(url='https://qauto2.forstudy.space/api/cars')
         response = result_car_id.json()["data"]
+        car_id = 0
         for item in response:
             car_id = item['id']
-        # delete car by car id
         delete_car_url = f"https://qauto2.forstudy.space/api/cars/{car_id}"
         delete_car = self.session.delete(url=delete_car_url)
         assert delete_car.json()["status"] == "ok"
-        # delete current user
+
+    def test_delete_user(self):
         delete_user = self.session.delete(url="https://qauto2.forstudy.space/api/users/")
         assert delete_user.json()["status"] == "ok"
